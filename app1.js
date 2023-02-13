532c3038a48fab64918eb8d393e0bb236ec5b0a3
Anirudha <110011181+anirudhaf18@users.noreply.github.com>
2023-02-13 22:37:49 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -346,7 +346,7 @@ function calculate() {
     // ************************************/
 
     let sgpa = document.getElementById("demo");
-    sgpa.innerText = "Your sgpa:- " + sum / 22 + "  Back:- " + backlog;
+    sgpa.innerText = "Your sgpa:- " + Math.floor(sum / 22) + "  Back:- " + backlog;
 
     sum = 0;
     backlog = 0;
